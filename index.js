9efabe85e8a7df26833e272511393a5d6ebe8a18
Lucas Hugdahl <lucashugdahl@Lucass-MacBook-Air.local>
2021-11-29 17:02:03 -0800

    fix: build

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const sass = require("sass");
 const fs = require("fs");
 const path = require("path");
 
-const file = path.join(__dirname, "main.scss");
+const file = path.join(__dirname, "/src/main.scss");
 const outPath = path.join(__dirname, "main.css");
 
 const { css } = sass.renderSync({ file });
 
 fs.writeFileSync(outPath, css);
 
-console.log(`- compiled: \n   ${file} -> ${outPath}\n`)
+module.exports = () => css.toString();
